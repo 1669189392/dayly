20443f6e0cd950c37cd424880941452496ee8cba
崔煜杰 <1669189392@qq.com>
2021-08-04 00:01:14 +0800

    text

--- numpy—array合并.py
+++ numpy—array合并.py
@@ -8,3 +8,8 @@ print(np.hstack((a, b)))  # a,b横向合并
 print(a[:, np.newaxis])  # np.newaxis 新增一个维度（这一句增加的是列竖向维度）
 c = np.concatenate((a, b, a, b, a, b), axis=0)  # 多个矩阵进行合并，自选方向
 print(c)
+# 以下是矩阵纵置合并
+A = np.array([1, 1, 1])[:, np.newaxis]
+B = np.array([2, 2, 2])[:, np.newaxis]
+C = np.concatenate((A, B, A, B), axis=1)
+print(C)
